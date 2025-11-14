441418ba3750a054516e75205966dfb661255182
KrishnoMD305 <krishnomd60@gmail.com>
2025-11-14 18:19:48 +0600

    create attribute in product

--- main.cpp
+++ main.cpp
@@ -2,7 +2,15 @@
 using namespace std;
 
 
-
+class Product{
+protected:
+    // attributes for products
+    int id;
+    string name;
+    float price;
+    int stock;
+    static int totalProducts; // it should be shared by all objects that's why static
+};
 // Main system class for everything
 class ECommerceSystem{
 
