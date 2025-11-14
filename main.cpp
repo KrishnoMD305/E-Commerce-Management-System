2e81df5e39324d38e5f0dbaff856338b9dab68b7
KrishnoMD305 <krishnomd60@gmail.com>
2025-11-14 18:23:37 +0600

    make constructor in product

--- main.cpp
+++ main.cpp
@@ -10,6 +10,15 @@ protected:
     float price;
     int stock;
     static int totalProducts; // it should be shared by all objects that's why static
+
+public:
+    // Constructor
+    Product(int i, string n, float p, int s): id(i), name(n), price(p), stock(s){
+        totalProducts++; // increases static counter every time a new product is creted
+    }
+
+
+
 };
 // Main system class for everything
 class ECommerceSystem{
